caad642d8ca2d6bf69d8de65bcb7b6c5888eda7f
Dwyer <dwyero@lancs.local>
2017-02-15 16:33:28 +0000

    Spaceship moves on keypress

--- Game.java
+++ Game.java
@@ -3,9 +3,27 @@ public class Game {
 	public static void main(String[] args){
 		GameArena game1 = new GameArena(400,400);
 		
-		Spaceship ship1 = new Spaceship(10,10);
+		Spaceship ship1 = new Spaceship(200,200);
 		ship1.addTo(game1);
 		
+		
+		while (true){
+			game1.pause();		
+			
+			
+			if (game1.upPressed()) {
+				ship1.move(0,-2);
+			}
+			if (game1.downPressed()) {
+				ship1.move(0,2);
+			}
+			if (game1.leftPressed()) {
+				ship1.move(-2,0);
+			}
+			if (game1.rightPressed()) {
+				ship1.move(2,0);
+			}
+		}
 		
 		
 	}
--- Spaceship.java
+++ Spaceship.java
@@ -5,19 +5,32 @@ public class Spaceship {
 	private Rectangle[] rects = new Rectangle[5];
 	
 	public Spaceship(int xPosition, int yPosition) {
-		
-		
+				
+		/*
 		rects[0] = new Rectangle(175,205,10,30, "GREEN");
 		rects[1] = new Rectangle(180,200,20,10, "GREEN");
 		rects[2] = new Rectangle(190,195,15,30, "GREEN");
 		rects[3] = new Rectangle(200,200,10,10, "GREEN");
 		rects[4] = new Rectangle(205,205,10,30, "GREEN");
-
+		*/
+		rects[0] = new Rectangle((xPosition-25),(xPosition+5),10,30, "GREEN");
+		rects[1] = new Rectangle((xPosition-20),(xPosition  ),20,10, "GREEN");
+		rects[2] = new Rectangle((xPosition-10),(xPosition-5),15,30, "GREEN");
+		rects[3] = new Rectangle((xPosition   ),(xPosition  ),10,10, "GREEN");
+		rects[4] = new Rectangle((xPosition+5 ),(xPosition+5),10,30, "GREEN");
 	}
 	
 	public void addTo(GameArena game) {
 		for (int i =0; i < rects.length; i++){
 			game.addRectangle(rects[i]);
+		}
+	}
+	
+	
+	public void move(int xAmount, int yAmount) {
+		for (int i=0; i < rects.length; i++) {
+			rects[i].setXPosition( rects[i].getXPosition() + xAmount ) ;
+			rects[i].setYPosition( rects[i].getYPosition() + yAmount ) ;
 		}
 	}
 	
